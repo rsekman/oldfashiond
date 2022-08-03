7a311fed5ebd4e8ee81a78f4be8a6371ec5c7fc1
Robin Ekman <robin.seth.ekman@gmail.com>
2022-08-04 01:50:52 +0200

    add features to seek through subtitle indices

--- gifmake.py
+++ gifmake.py
@@ -1,7 +1,9 @@
 #!/usr/bin/python
 
 import argparse, subprocess, pathlib, tempfile, sys
-from datetime import datetime
+from enum import Enum
+from collections import OrderedDict
+from datetime import datetime, timedelta
 
 try:
     import humanfriendly
@@ -18,25 +20,145 @@ except ModuleNotFoundError:
         return datetime.fromtimestamp(mtime).strftime("%c%")
 
 
+SubFormat = Enum("SubFormat", "ASS SRT")
+
+
+def determine_sub_format(fname):
+    """
+    Returns a SubFormat corresponding to the type of the subtitle file fname.
+    Raises ValueError if it is not a supported format, or the required
+    dependency is not present.
+    """
+
+    def is_srt(f):
+        try:
+            from srt import parse, SRTParseError
+
+            parse(f, ignore_errors=False)
+        except (ImportError, SRTParseError):
+            return False
+        else:
+            return True
+
+    def is_ass(f):
+        try:
+            from ass import parse
+
+            parse(f)
+        except (ImportError, ValueError) as e:
+            print(e)
+            return False
+        else:
+            return True
+
+    fmt_tests = OrderedDict([(SubFormat.ASS, is_ass), (SubFormat.SRT, is_srt)])
+    with open(fname, encoding='utf-8-sig') as f:
+        for (fmt, test) in fmt_tests.items():
+            if test(f):
+                return fmt
+            f.seek(0)
+
+    raise ValueError(f"{fname} is not a supported subtitle file.")
+
+
+def get_nth_sub_line(fname, n):
+    """
+    Returns a tuple (start, end) of the start and end times of the n:th line in
+    the subtitle file fname. Both times are datetime.timedelta objects.
+    Attempts to parse the file first as SRT, then as ASS. Raises IndexError if
+    n is out of bounds and ValueError if the file is not valid SRT or ASS.
+    """
+    with open(fname, encoding="utf_8_sig") as f:
+        # Try to read as SRT
+        try:
+            from srt import parse, SRTParseError
+
+            for line in parse(f, ignore_errors=False):
+                if line.index == n:
+                    return (line.start, line.end)
+            raise IndexError(f"Could not find line {n} in {fname}.")
+
+        except (ImportError, SRTParseError):
+            pass
+        f.seek(0)
+        try:
+            from ass import parse
+
+            sub = parse(f)
+            # avoid off-by-one error: Aegiusub is 1-indexed
+            line = sub.events[n - 1]
+            return (line.start, line.end)
+        except IndexError:
+            raise IndexError("Could not find line {n} in {fname}.")
+        except (ImportError, ValueError) as e:
+            pass
+        raise ValueError(f"{fname} is not a supported subtitle file.")
+
+
 gifmake_args = argparse.ArgumentParser()
-gifmake_args.add_argument(
-    "-ss", help="Start reading at %(metavar)s.", required=True, metavar="SS"
+time_group = gifmake_args.add_argument_group("Timing options")
+start_group = time_group.add_mutually_exclusive_group(required=True)
+start_group.add_argument("-ss", help="Start reading at %(metavar)s.", metavar="SS")
+sub_dep_doc = """
+    This requires parsing the subtitle file and depends on the srt and ass libraries for SRT and ASS files, respectively.
+    To find the index of a line in an SRT file, run fgrep --before-context 3
+    "search string" subs.srt. To find the index of a line in an ASS file, open
+    it in Aegisub.
+"""
+start_group.add_argument(
+    "--sub-line-start",
+    help=f"Start reading at the beginning of the N:th line of the subtitle file. {sub_dep_doc}",
+    type=int,
+    default=None,
+    metavar="N",
 )
-stop_group = gifmake_args.add_mutually_exclusive_group(required=True)
-stop_group.add_argument("-to", help="Read up to %(metavar)s.", metavar="TO")
-stop_group.add_argument("-t", help="Read for %(metavar)s.", metavar="T")
+stop_group = time_group.add_mutually_exclusive_group(required=True)
+stop_group.add_argument(
+    "-to",
+    help="Read up to %(metavar)s. Mutually exclusive with -t and --sub-line-end.",
+    metavar="TO",
+)
+stop_group.add_argument(
+    "-t",
+    help="Read for %(metavar)s. Mutually exclusive with -to and --sub-line-end.",
+    metavar="T",
+)
+stop_group.add_argument(
+    "--sub-line-end",
+    help=f"Stop reading from at the end of the N:th subtitle line. Mutually exclusive with -t and -to. {sub_dep_doc}",
+    default=None,
+    type=int,
+    metavar="N",
+)
 sub_group = gifmake_args.add_argument_group("Subtitle options")
 sub_group.add_argument(
     "--sub-file",
-    help="Hardsub the gif using subtitles from %(metavar)s. Requires an ffmpeg compiled with libass.",
+    help="""Hardsub the gif using subtitles from %(metavar)s.
+    Requires an ffmpeg compiled with --enable-libass.
+    """,
     default=None,
     metavar="SUBFILE",
 )
 sub_group.add_argument(
+    "--no-sub",
+    help="Do NOT hardsub the gif even though --sub-file was passed. The use case for this is to use a subtitle file purely for timing with --sub-line-start and --sub-line-end",
+    action="store_true",
+)
+sub_group.add_argument(
     "--sub-style",
-    help="Use the %(metavar)s ASS style for subtitles. Default: %(default)s.",
+    help="Use the %(metavar)s ASS style for SRT subtitles. Has no effect for ASS subtitles. Default: %(default)s.",
     default="Fontsize=24,Outline=2",
     metavar="STYLE",
+)
+sub_group.add_argument(
+    "--sub-lead-in",
+    help="""
+    Allow for %(metavar)s ms of lead-in. Increase this if there are extraneous frames at the beginning of the gif
+    (default: %(default)s)
+    """,
+    default=80,
+    type=int,
+    metavar="T",
 )
 filter_group = gifmake_args.add_mutually_exclusive_group()
 filter_group.add_argument(
@@ -62,13 +184,14 @@ gifmake_args.add_argument(
     help="Force regeneration of the palette.",
     action="store_true",
 )
-gifmake_args.add_argument(
+log_group = gifmake_args.add_argument_group("Logging options")
+log_group.add_argument(
     "-v",
     "--verbose",
     help="Verbose mode. Prints extra information. Not mutually exclusive with -q, which suppresses ffmpeg output.",
     action="store_true",
 )
-gifmake_args.add_argument(
+log_group.add_argument(
     "-q",
     "--quiet",
     help="Quiet mode. Passes -hide_banner -loglevel warning to ffmpeg. Not mutually exclusive with -v.",
@@ -78,6 +201,10 @@ gifmake_args.add_argument("input")
 gifmake_args.add_argument("output")
 
 args = gifmake_args.parse_args()
+
+if (args.sub_line_start or args.sub_line_end) and not args.sub_file:
+    sys.exit("error: passing --sub-line-start or --sub-line-end requires --sub-file")
+
 if args.verbose:
     logger = print
 else:
@@ -91,11 +218,22 @@ ffmpeg_args = []
 ffmpeg_args += ["-stats"]
 if args.quiet:
     ffmpeg_args += ["-hide_banner", "-loglevel", "warning"]
-ffmpeg_args += ["-ss", args.ss, "-copyts"]
+
+if args.ss:
+    start_time = args.ss
+elif args.sub_line_start:
+    times = get_nth_sub_line(args.sub_file, args.sub_line_start)
+    lead_in = timedelta(microseconds=args.sub_lead_in * 1e3)
+    start_time = str(times[0] + lead_in)
+
+ffmpeg_args += ["-ss", start_time, "-copyts"]
 if args.to:
     ffmpeg_args += ["-to", args.to]
-else:
+elif args.t:
     ffmpeg_args += ["-t", args.t]
+elif args.sub_line_end:
+    times = get_nth_sub_line(args.sub_file, args.sub_line_end)
+    ffmpeg_args += ["-to", str(times[1])]
 ffmpeg_args += ["-i", args.input]
 
 # -r needs to be an output argument to not mess up seeking!
@@ -103,7 +241,7 @@ ffmpeg_output_args = ["-r", str(args.rate)]
 if args.sub_file is not None:
     # to sync subtitles we have to -copyts and seek twice, see
     # https://trac.ffmpeg.org/wiki/HowToBurnSubtitlesIntoVideo
-    ffmpeg_output_args += ["-ss", args.ss]
+    ffmpeg_output_args += ["-ss", start_time]
 
 if args.width:
     args.filters = "[0:v] scale=%s:-1:flags=lanczos" % args.width
@@ -114,7 +252,11 @@ else:
     palette_filtergraph = "%s, palettegen" % args.filters
 
 if args.sub_file is not None:
-    sub_filter = f"subtitles={args.sub_file}:force_style='{args.sub_style}'"
+    sub_fmt = determine_sub_format(args.sub_file)
+    if sub_fmt == SubFormat.SRT:
+        sub_filter = f"subtitles={args.sub_file}:force_style='{args.sub_style}'"
+    elif sub_fmt == SubFormat.ASS:
+        sub_filter = f"ass={args.sub_file}"
 else:
     sub_filter = "copy"
 
